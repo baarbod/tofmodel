9e93e1c4e0b9a605e4c560c2c427d1db9a68014a
Baarbod <baarbod.ashen@gmail.com>
2023-01-18 15:18:41 -0500

    refactored and add parameter dict

--- tofmodel.py
+++ tofmodel.py
@@ -56,95 +56,112 @@ def fre_signal(n, fa, TR, T1, dt_list):
     S = np.sin(fa)*(Mzn_pre - Mzss)
     return S
 
-# Scan parameters (should be function input)
-w = 0.25
-TR = 0.35
-fa = 47*math.pi/180
-T1 = 4
-nslice = 5
-npulse = 100
-alpha_list = np.array([0.14, 0, 0.2075, 0.07, 0.2775], ndmin=2).T
+def run_tof_model(v0, scan_param):
+    
+    # Scan parameters (should be function input)
+    w = scan_param['slice_width']
+    TR = scan_param['repetition_time']
+    fa = scan_param['flip_angle']*math.pi/180
+    T1 = scan_param['t1_time']
+    nslice = scan_param['num_slice']
+    npulse = scan_param['num_pulse']
+    alpha = np.array(scan_param['alpha_list'], ndmin=2).T
 
-# Simulation time vector
-div = 20
-tmax = TR*npulse
-t = np.arange(0, tmax, TR/div)
+    # Simulation time vector
+    div = 20
+    tmax = TR*npulse
+    t = np.arange(0, tmax, TR/div)
+    
+    # Initialize protons for simulation
+    x0 = 0
+    X = compute_position(t, x0, v0)
+    dx = 0.01
+    X0array = np.arange(-max(X), nslice*w, dx)
+    nproton = np.size(X0array)
+    nproton_per_slice = int(w / dx)
+    
+    # Define list of tuples defining pulse timings and corresponding target slices
+    tr_vect = np.array(range(npulse))*TR
+    T = tr_vect + alpha
+    T = T.T
+    pulse_target_slice = np.repeat(np.array(range(nslice)), npulse)
+    pulse_timing = np.reshape(T.T, npulse*nslice)
+    pulse_slice_tuple = [(pulse_timing[i], pulse_target_slice[i]) 
+                         for i in range(0, len(pulse_timing))]
+    pulse_slice_tuple = sorted(pulse_slice_tuple)
+    
+    # Unpack timing-slice tuple
+    pulse_slice_tuple_unpacked = list(zip(*pulse_slice_tuple))
+    timings = np.array(pulse_slice_tuple_unpacked[0])
+    pulse_slice = np.array(pulse_slice_tuple_unpacked[1])
+    
+    # Define array of TR cycles that each pulse belongs to
+    pulse_tr_actual = []
+    for ipulse in range(npulse):
+        tr_block = ipulse*np.ones(nslice)
+        pulse_tr_actual = np.append(pulse_tr_actual, tr_block)    
+    pulse_tr_actual = pulse_tr_actual.astype(int)
+    
+    t = time.time()
+    
+    signal = np.zeros([npulse, nslice])
+    s_counter = np.zeros([npulse, nslice])
+    for iproton in range(nproton):
+        
+        # Solve position at each pulse for this proton
+        init_pos = X0array[iproton]
+        proton_position = compute_position(timings, init_pos, v0)
+        
+        # Convert absolute positions to slice location
+        proton_slice = np.floor(proton_position/w)
+        
+        # Find pulses that this proton recieved (i.e. in a slice that recieved one)
+        match_cond = pulse_slice == proton_slice
+        pulse_recieve_ind = np.where(match_cond)[0]
+        
+        # Loop through recieved pulses and compute flow signal
+        tprev = float('nan')
+        dt_list = np.empty(np.size(pulse_recieve_ind))
+        for count, pulse_id in enumerate(pulse_recieve_ind):
+             tnow = timings[pulse_id]
+             dt = tnow - tprev # correct dt behavior on 1st pulse
+             tprev = tnow
+             dt_list[count] = dt
+             npulse = count+1 # correcting for zero-based numbering
+             S = fre_signal(npulse, fa, TR, T1, dt_list)
+             current_tr = pulse_tr_actual[pulse_id]
+             current_slice = proton_slice[pulse_id]
+             current_slice = current_slice.astype(int)
+             signal[current_tr, current_slice] += S
+             s_counter[current_tr, current_slice] += 1
+    
+    elapsed = time.time() - t
+    print(elapsed)
+    
+    # Check conservation of protons
+    err_statement = 'Warning: proton conservation failed - check s_counter.'
+    assert np.all(s_counter == nproton_per_slice), err_statement
+        
+    # Divide after summing contributions; signal is the average of protons
+    signal = signal / nproton_per_slice    
+    return signal
 
-# Constant velocity
+
+# Velocity input amplitude
 v0 = 0.7
 
-# Initialize protons for simulation
-x0 = 0
-X = compute_position(t, x0, v0)
-dx = 0.01
-X0array = np.arange(-max(X), nslice*w, dx)
-nproton = np.size(X0array)
-nproton_per_slice = int(w / dx)
+# Dictionary containing model parameters
+scan_param =	{
+    'slice_width' : 0.25,
+    'repetition_time' : 0.35,
+    'flip_angle' : 47,
+    't1_time' : 4,
+    'num_slice' : 5,
+    'num_pulse' : 100,
+    'alpha_list' : [0.14, 0, 0.2075, 0.07, 0.2775]}
 
-# Define list of tuples defining pulse timings and corresponding target slices
-tr_vect = np.array(range(npulse))*TR
-T = tr_vect + alpha_list
-T = T.T
-pulse_target_slice = np.repeat(np.array(range(nslice)), npulse)
-pulse_timing = np.reshape(T.T, npulse*nslice)
-pulse_slice_tuple = [(pulse_timing[i], pulse_target_slice[i]) 
-                     for i in range(0, len(pulse_timing))]
-pulse_slice_tuple = sorted(pulse_slice_tuple)
+signal = run_tof_model(v0, scan_param)
 
-# Unpack timing-slice tuple
-pulse_slice_tuple_unpacked = list(zip(*pulse_slice_tuple))
-timings = np.array(pulse_slice_tuple_unpacked[0])
-pulse_slice = np.array(pulse_slice_tuple_unpacked[1])
-
-# Define array of TR cycles that each pulse belongs to
-pulse_tr_actual = []
-for ipulse in range(npulse):
-    tr_block = ipulse*np.ones(nslice)
-    pulse_tr_actual = np.append(pulse_tr_actual, tr_block)    
-pulse_tr_actual = pulse_tr_actual.astype(int)
-
-t = time.time()
-
-signal = np.zeros([npulse, nslice])
-s_counter = np.zeros([npulse, nslice])
-for iproton in range(nproton):
-    
-    # Solve position at each pulse for this proton
-    init_pos = X0array[iproton]
-    proton_position = compute_position(timings, init_pos, v0)
-    
-    # Convert absolute positions to slice location
-    proton_slice = np.floor(proton_position/w)
-    
-    # Find pulses that this proton recieved (i.e. in a slice that recieved one)
-    match_cond = pulse_slice == proton_slice
-    pulse_recieve_ind = np.where(match_cond)[0]
-    
-    # Loop through recieved pulses and compute flow signal
-    tprev = float('nan')
-    dt_list = np.empty(np.size(pulse_recieve_ind))
-    for count, pulse_id in enumerate(pulse_recieve_ind):
-         tnow = timings[pulse_id]
-         dt = tnow - tprev # correct dt behavior on 1st pulse
-         tprev = tnow
-         dt_list[count] = dt
-         npulse = count+1 # correcting for zero-based numbering
-         S = fre_signal(npulse, fa, TR, T1, dt_list)
-         current_tr = pulse_tr_actual[pulse_id]
-         current_slice = proton_slice[pulse_id]
-         current_slice = current_slice.astype(int)
-         signal[current_tr, current_slice] += S
-         s_counter[current_tr, current_slice] += 1
-
-elapsed = time.time() - t
-print(elapsed)
-
-# Check conservation of protons
-if np.any(s_counter > nproton_per_slice):
-    print('Warning: proton conservation failed - check s_counter.')
-
-# Divide after summing contributions; signal is the average of protons
-signal = signal / nproton_per_slice    
 
 # Plot slice signals
 plt.plot(signal)
