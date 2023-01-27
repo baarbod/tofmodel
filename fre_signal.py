302f72cbd9c1299edd7bab4fe38f190a3d36c7bd
Lewis Lab <lewislab@RKC-LEW-WD-0002.ad.bu.edu>
2023-01-27 17:31:15 -0500

    started optimizing the prod usage

--- fre_signal.py
+++ fre_signal.py
@@ -6,6 +6,10 @@ Created on Wed Jan 25 15:48:37 2023
 """
 
 import numpy as np
+#import math 
+import operator
+import functools
+
 
 # Define equation for flow-enhanced fMRI signal
 def fre_signal(n, fa, TR, T1, dt_list):
@@ -19,16 +23,29 @@ def fre_signal(n, fa, TR, T1, dt_list):
     negative_series = np.zeros(n-1)
     E = np.exp(-dt_list[1:]/T1)
     
+    
+    # CAN BE OPTIMIZED. WHY IS MATLAB FASTER? IS IT THE PRODUCT?
+    num = functools.reduce(operator.mul, E[0:n-1], 1)
+    
     if n == 1:
         Mzn_pre = M0*C
     else:
         for m in range(n):
-            positive_series[m] = C**m * np.prod(E[0:n-1])/np.prod(E[0:n-m-1]) 
+            #num = functools.reduce(operator.mul, E[0:n-1], 1)
+            den = functools.reduce(operator.mul, E[0:n-m-1], 1)
+            positive_series[m] = C**m * num / den
+            #positive_series[m] = C**m * np.prod(E[0:n-1])/np.prod(E[0:n-m-1]) 
+            #positive_series[m] = C**m * math.prod(E[0:n-1])/np.prod(E[0:n-m-1]) 
+            
         for m in range(n-1):
-            negative_series[m] = C**m * np.prod(E[0:n-1])/np.prod(E[0:n-m-2])
+            #num = functools.reduce(operator.mul, E[0:n-1], 1)
+            den = functools.reduce(operator.mul, E[0:n-m-2], 1)
+            negative_series[m] = C**m * num / den            
+            #negative_series[m] = C**m * np.prod(E[0:n-1])/np.prod(E[0:n-m-2])
+            #negative_series[m] = C**m * math.prod(E[0:n-1])/np.prod(E[0:n-m-2])
+            
         Mzn_pre = M0 * (np.sum(positive_series) - np.sum(negative_series))
         
     S = np.sin(fa)*(Mzn_pre - Mzss)
     return S
-
 
