b92d2eece27aa7ec95f76ac14b14fca0b1dedfaa
Baarbod <baarbod.ashen@gmail.com>
2023-02-20 11:39:15 -0500

    new array fre method implemented

--- fre_signal.py
+++ fre_signal.py
@@ -6,15 +6,14 @@ Created on Wed Jan 25 15:48:37 2023
 """
 
 import numpy as np
-#import math 
 import operator
 import functools
 
+# array version can run faster for slower flows, but perform same or worse for 
+# faster flows.
+
 # Define equation for flow-enhanced fMRI signal
 def fre_signal(n, fa, TR, T1, dt_list):
-    # e1 = math.exp(-TR/T1)
-    # S = math.sin(fa)*(e1*math.cos(fa))**(n-1)*(1 - (1-e1)/(1-e1*math.cos(fa)))
-    
     
     M0 = 1
     C = np.cos(fa)
@@ -22,9 +21,7 @@ def fre_signal(n, fa, TR, T1, dt_list):
     series = np.zeros(n-1)
     E = np.exp(-dt_list/T1)
     
-    
     # try storing each series in matrix and do prod once at the end
-    
     E_full = functools.reduce(operator.mul, E[1:n], 1)
     for m in range(n-1):
         num = 1 - E[n-m-1]
@@ -35,6 +32,37 @@ def fre_signal(n, fa, TR, T1, dt_list):
     Mzn_pre = M0 * E_full * (np.sum(series) + C**(n-1))
         
     S = np.sin(fa)*(Mzn_pre - Mzss)
+    
+    return S
+
+# Define equation for flow-enhanced fMRI signal usig array method
+def fre_signal_array(n, fa, TR, T1, dt_list):
+    
+    M0 = 1
+    C = np.cos(fa)
+    Mzss = M0*(1 - np.exp(-TR/T1)) / (1 - np.exp(-TR/T1)*C)
+    E = np.exp(-dt_list/T1)
+    
+    E_full = functools.reduce(operator.mul, E[1:n], 1)
+    
+    # put all terms in array
+    full_array = np.ones((n-1, n-1))
+    for m in range(n-1):
+        full_array[m, 0:n-m-1] = E[1:n-m]       
+        
+    if n == 1:
+        series = 0
+        Mzn_pre = M0 * E_full * (np.sum(series) + C**(n-1))
+        S = np.sin(fa)*(Mzn_pre - Mzss)
+    else:    
+        # call prod function on full array
+        full_array_prod = functools.reduce(operator.mul, full_array)
+        mm = np.arange(0, n-1)
+        nummat = 1 - E[n - mm - 1]
+        final_mat = C**mm * nummat/full_array_prod
+        series = final_mat
+        Mzn_pre = M0 * E_full * (np.sum(series) + C**(n-1))
+        S = np.sin(fa)*(Mzn_pre - Mzss)
     
     return S
 
--- tofmodel.py
+++ tofmodel.py
@@ -12,7 +12,8 @@ import matplotlib.pyplot as plt
 import posfunclib as pfl
 from functools import partial 
 import input_handler as ih
-from fre_signal import fre_signal
+#from fre_signal import fre_signal
+from fre_signal import fre_signal_array as fre_signal
 
 def run_tof_model(scan_param, Xfunc):
     
@@ -105,7 +106,7 @@ def run_tof_model(scan_param, Xfunc):
 
 # Position functions
 #Xfunc = partial(pfl.compute_position_constant, v0=0.1)
-Xfunc = partial(pfl.compute_position_sine, v1=0, v2=0.5, w0=2*np.pi/5)
+Xfunc = partial(pfl.compute_position_sine, v1=0, v2=0.4, w0=2*np.pi/5)
 
 # An = [0.2, 0.1]
 # Bn = [0*0.1]
