bd944f123fbecdd31493212e92fbc1b5648ac19e
Baarbod <baarbod.ashen@gmail.com>
2023-02-21 15:05:11 -0500

    separated tofmodel test code from the main function

--- tofmodel.py
+++ tofmodel.py
@@ -8,10 +8,6 @@ Created on Fri Jan 13 10:41:44 2023
 import math
 import time
 import numpy as np
-import matplotlib.pyplot as plt
-import posfunclib as pfl
-from functools import partial 
-import input_handler as ih
 #from fre_signal import fre_signal
 from fre_signal import fre_signal_array as fre_signal
 
@@ -104,35 +100,6 @@ def run_tof_model(scan_param, Xfunc):
     signal = signal / nproton_per_slice    
     return signal
 
-
-# Position functions
-#Xfunc = partial(pfl.compute_position_constant, v0=0.1)
-#Xfunc = partial(pfl.compute_position_sine, v1=-0.5, v2=0.5, w0=2*np.pi/5)
-#Xfunc = partial(pfl.compute_position_sine, v1=-0.5, v2=0.5, w0=2*np.pi/5)
-Xfunc = partial(pfl.compute_position_sine_spatial, v1=-0.5, v2=0.5, w0=2*np.pi/5)
-
-# An = [0.2, 0.1]
-# Bn = [0*0.1]
-# w0 = 2*np.pi/5
-# Xfunc = partial(pfl.compute_position_fourier, An=An, Bn=Bn, w0=w0)
-
-#scan_param = ih.input_from_json('test_scan.json')
-
-# Dictionary containing model parameters
-scan_param =	{
-    'slice_width' : 0.25,
-    'repetition_time' : 0.35,
-    'flip_angle' : 47,
-    't1_time' : 4,
-    'num_slice' : 10,
-    'num_pulse' : 100,
-    'alpha_list' : [0.14, 0, 0.2075, 0.07, 0.2775, 0.14, 0, 0.2075, 0.07, 0.2775]}
-
-signal = run_tof_model(scan_param, Xfunc)
-
-# Plot slice signals
-plt.plot(signal[:, 0:4])
-plt.show()
 
 
 
