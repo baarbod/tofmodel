fe33538e2ee0618bc90b36e8821e860868e5731f
Baarbod <baarbod.ashen@gmail.com>
2023-03-09 20:20:04 -0500

    changed proton initialized

--- tof/tofmodel.py
+++ tof/tofmodel.py
@@ -47,7 +47,7 @@ def set_init_positions(Xfunc, TR, w, npulse, nslice, dx):
     dummyt = np.arange(0, TR*npulse, TR/20)
     x0 = 0
     X = Xfunc(dummyt, x0)
-    X0array = np.arange(-max(X), max(X), dx)
+    X0array = np.arange(-max(X), 2*nslice*w, dx)
     return X0array
     
 
