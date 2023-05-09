92e796a06d2f04a33d06b89fa0c7e83a7b384447
Baarbod <baarbod.ashen@gmail.com>
2023-05-08 22:11:04 -0400

    removed header comment

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Mar  7 16:13:47 2023
-
-@author: Baarbod
-"""
 
 from setuptools import find_packages, setup
 
--- tof/__init__.py
+++ tof/__init__.py
@@ -1,7 +1,2 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Feb 21 13:12:44 2023
-
-@author: Baarbod
-"""
 
