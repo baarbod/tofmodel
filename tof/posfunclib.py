18973ef918faa122200991bb5cf3b0de1229a4e6
Baarbod <baarbod.ashen@gmail.com>
2023-05-08 22:08:41 -0400

    updated area implementation for fourier function

--- tof/posfunclib.py
+++ tof/posfunclib.py
@@ -1,13 +1,7 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 18 16:24:53 2023
-
-@author: Baarbod
-"""
 
 import numpy as np
 from scipy.integrate import solve_ivp
-from scipy.integrate import cumtrapz
 
 def model(Xfunc, t):
     x = Xfunc(t, 0)
@@ -22,26 +16,6 @@ def compute_position_sine(t, x0, v1, v2, w0):
     A0 = (v1 + Amp)*2
     An = Amp
     return A0*t/2 + An/w0*np.sin(w0*t) + x0 
-    
-def compute_position_fourier(t, x0, An, Bn, w0):
-    
-    A0 = np.array(An[0])
-    An = np.array(An[1:])
-    Bn = np.array(Bn)
-    N = np.size(An)
-    Nvect = np.ones((N, 1))
-    
-    An = np.reshape(An, (N, 1))
-    Bn = np.reshape(Bn, (N, 1))
-    w0 = np.reshape(w0, (N, 1))
-    k = np.sum(Bn/(w0*Nvect)) + x0
-    if N == 1:
-        term = An/w0*np.sin(w0*t) - Bn/w0*np.cos(w0*t)
-    else:
-        tt = An/(w0*Nvect)*np.sin(w0*Nvect*t) - Bn/(w0*Nvect)*np.cos(w0*Nvect*t)
-        term = np.sum(tt, axis=0)
-    term = term.squeeze()
-    return A0*t/2 + term + k
 
 def compute_position_sine_spatial(t_eval, x0, v1, v2, w0, xarea, A):
 
@@ -65,9 +39,35 @@ def compute_position_sine_spatial(t_eval, x0, v1, v2, w0, xarea, A):
     sol = solve_ivp(F, trange, [x0], args=p, t_eval=t_eval)
     return  sol.y[0]
 
-def compute_position_fourier_spatial(t_eval, x0, An, Bn, w0):
+def compute_position_fourier(t, x0, An, Bn, w0):
     
-    def F(t, x, k, m, r1, An, Bn, w0):
+    A0 = np.array(An[0])
+    An = np.array(An[1:])
+    Bn = np.array(Bn)
+    N = np.size(An)
+    Nvect = np.ones((N, 1))
+    
+    An = np.reshape(An, (N, 1))
+    Bn = np.reshape(Bn, (N, 1))
+    w0 = np.reshape(w0, (N, 1))
+    k = np.sum(Bn/(w0*Nvect)) + x0
+    if N == 1:
+        term = An/w0*np.sin(w0*t) - Bn/w0*np.cos(w0*t)
+    else:
+        tt = An/(w0*Nvect)*np.sin(w0*Nvect*t) - Bn/(w0*Nvect)*np.cos(w0*Nvect*t)
+        term = np.sum(tt, axis=0)
+    term = term.squeeze()
+    return A0*t/2 + term + k
+
+def compute_position_fourier_spatial(t_eval, x0, An, Bn, w0, xarea, A):
+    
+    def F(t, x, An, Bn, w0, xarea, A):
+        ind0 = xarea == 0
+        area0 = A[ind0]
+        diffarray = np.absolute(xarea-x)
+        ind = diffarray.argmin()
+        pos_term = area0/A[ind]
+        
         A0 = np.array(An[0])
         An = np.array(An[1:])
         Bn = np.array(Bn)
@@ -85,16 +85,12 @@ def compute_position_fourier_spatial(t_eval, x0, An, Bn, w0):
             term = np.sum(tt, axis=0)
         term = term.squeeze()
         
-        pos_term = k*(r1/(m*x + r1))**2
-        pos_term = np.heaviside(x, 0)*pos_term + np.heaviside(-x, 1)    
         time_term = A0 + term  + kk
         state = pos_term * time_term 
         return state
     
-    k = 1
-    m = 0.1
-    r1 = 1
-    p = (k, m, r1, An, Bn, w0)
+    
+    p = (An, Bn, w0, xarea, A)
     
     trange = [np.min(t_eval), np.max(t_eval)]
     sol = solve_ivp(F, trange, [x0], args=p, t_eval=t_eval, method='Radau')
