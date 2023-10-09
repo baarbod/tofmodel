0aeaf9deb9917c3622865cea6295e6ae035101f5
Baarbod <baarbod.ashen@gmail.com>
2023-10-09 16:39:05 -0400

    reformatted

--- tof/fresignal.py
+++ tof/fresignal.py
@@ -4,60 +4,59 @@ import numpy as np
 import operator
 import functools
 
-# array version can run faster for slower flows, but perform same or worse for 
+
+# array version can run faster for slower flows, but perform same or worse for
 # faster flows.
 
 # Define equation for flow-enhanced fMRI signal
-def fre_signal(n, fa, TR, T1, dt_list):
-    
-    M0 = 1
-    C = np.cos(fa)
-    Mzss = M0*(1 - np.exp(-TR/T1)) / (1 - np.exp(-TR/T1)*C)
-    series = np.zeros(n-1)
-    E = np.exp(-dt_list/T1)
-    
-    # try storing each series in matrix and do prod once at the end
-    E_full = functools.reduce(operator.mul, E[1:n], 1)
-    for m in range(n-1):
-        num = 1 - E[n-m-1]
-        # removed the -1 from indexing in order to include last element
-        den = functools.reduce(operator.mul, E[1:n-m], 1)
-        series[m] = C**(m) * num/den
-                
-    Mzn_pre = M0 * E_full * (np.sum(series) + C**(n-1))
-        
-    S = np.sin(fa)*(Mzn_pre - Mzss)
-    
-    return S
+def fre_signal(n, fa, tr, t1, dt_list):
+    m0 = 1
+    c = np.cos(fa)
+    mz_ss = m0 * (1 - np.exp(-tr / t1)) / (1 - np.exp(-tr / t1) * c)
+    series = np.zeros(n - 1)
+    exponentials = np.exp(-dt_list / t1)
 
-# Define equation for flow-enhanced fMRI signal usig array method
-def fre_signal_array(n, fa, TR, T1, dt_list):
-    
-    M0 = 1
-    C = np.cos(fa)
-    Mzss = M0*(1 - np.exp(-TR/T1)) / (1 - np.exp(-TR/T1)*C)
-    E = np.exp(-dt_list/T1)
-    
-    E_full = functools.reduce(operator.mul, E[1:n], 1)
-    
+    # try storing each series in matrix and do prod once at the end
+    exponentials_full = functools.reduce(operator.mul, exponentials[1:n], 1)
+    for m in range(n - 1):
+        num = 1 - exponentials[n - m - 1]
+        # removed the -1 from indexing in order to include last element
+        den = functools.reduce(operator.mul, exponentials[1:n - m], 1)
+        series[m] = c ** m * num / den
+
+    mzn_pre = m0 * exponentials_full * (np.sum(series) + c ** (n - 1))
+
+    s = np.sin(fa) * (mzn_pre - mz_ss)
+
+    return s
+
+
+# Define equation for flow-enhanced fMRI signal using array method
+def fre_signal_array(n, fa, tr, t1, dt_list):
+    m0 = 1
+    c = np.cos(fa)
+    mz_ss = m0 * (1 - np.exp(-tr / t1)) / (1 - np.exp(-tr / t1) * c)
+    exponentials = np.exp(-dt_list / t1)
+
+    exponentials_full = functools.reduce(operator.mul, exponentials[1:n], 1)
+
     # put all terms in array
-    full_array = np.ones((n-1, n-1))
-    for m in range(n-1):
-        full_array[m, 0:n-m-1] = E[1:n-m]       
-        
+    full_array = np.ones((n - 1, n - 1))
+    for m in range(n - 1):
+        full_array[m, 0:n - m - 1] = exponentials[1:n - m]
+
     if n == 1:
         series = 0
-        Mzn_pre = M0 * E_full * (np.sum(series) + C**(n-1))
-        S = np.sin(fa)*(Mzn_pre - Mzss)
-    else:    
+        mzn_pre = m0 * exponentials_full * (np.sum(series) + c ** (n - 1))
+        s = np.sin(fa) * (mzn_pre - mz_ss)
+    else:
         # call prod function on full array
         full_array_prod = functools.reduce(operator.mul, full_array.T)
-        mm = np.arange(0, n-1)
-        nummat = 1 - E[n - mm - 1]
-        final_mat = C**mm * nummat/full_array_prod
+        mm = np.arange(0, n - 1)
+        nummat = 1 - exponentials[n - mm - 1]
+        final_mat = c ** mm * nummat / full_array_prod
         series = final_mat
-        Mzn_pre = M0 * E_full * (np.sum(series) + C**(n-1))
-        S = np.sin(fa)*(Mzn_pre - Mzss)
-    
-    return S
+        mzn_pre = m0 * exponentials_full * (np.sum(series) + c ** (n - 1))
+        s = np.sin(fa) * (mzn_pre - mz_ss)
 
+    return s
