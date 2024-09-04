b524e95e3b00e46d690f2a34a54cb2cbbf4b144c
Baarbod Ashenagar <baarbod.ashen@gmail.com>
2024-09-04 12:18:47 -0400

    implemented different method for defining input velocity with initial baseline period, added gaussian noise

--- tofinv/simulations.py
+++ tofinv/simulations.py
@@ -35,8 +35,14 @@ def run_model(x_func, scan_param, progress=False, showplot=False):
 def simulate_parameter_set(idx, input_data):
     
     # unpack tuple of inputs
-    frequencies, v_offset, rand_phase, velocity_input, scan_param, Xshape, Xtype, Yshape, Ytype, task_id, slc_offset, area_gauss_width, area_curve_fact = input_data
+    frequencies, v_offset, rand_phase, velocity_input, \
+    scan_param, Xshape, Xtype, Yshape, Ytype, task_id, \
+    slc_offset, area_gauss_width, area_curve_fact, gauss_noise_std = input_data
     
+    tr = scan_param['repetition_time']
+    npulse = scan_param["num_pulse"]
+    npulse_offset = scan_param["num_pulse_baseline_offset"]
+
     # get shared memory arrays 
     Xshm = msm.SharedMemory(name=f"Xarray{task_id}")
     Yshm = msm.SharedMemory(name=f"Yarray{task_id}")
@@ -49,46 +55,56 @@ def simulate_parameter_set(idx, input_data):
     N = lambda x, u, s: np.exp((-0.5) * ((x-u)/s)**2)
     area = area_curve_fact*N(xarea, xoffset, width)
     
-    # define position-time function based on parameter set
-    x_func = define_x_func(main_frequencies=frequencies, an_vals=[v_offset, *velocity_input], phase=rand_phase)
+    # define velocity based fourier series parameters
+    tdummy = tr * np.linspace(0, npulse, 2000)
+    vdummy = np.zeros(np.size(tdummy))
+    vdummy += v_offset
+    for amp, phase, w in zip(velocity_input, rand_phase, frequencies):
+        vsine = amp*np.cos(2*np.pi*w*(tdummy - phase))
+        vdummy += vsine
     
-    tdummy = np.linspace(0, 200, 1000)
-    xdummy = x_func(tdummy, 0)
-    vdummy = (np.diff(xdummy)/(tdummy[1]-tdummy[0])) 
-    x_func_area = partial(pfl.compute_position_numeric_spatial, tr_vect=tdummy[:999], vts=vdummy, xarea=xarea, area=area)
+    # add initial zero-flow period to establish baseline signal before starting flow
+    baseline_dt = tr*npulse_offset
+    dt = tdummy[1] - tdummy[0]
+    npoints_baseline = int(np.ceil(baseline_dt/dt))
+    vdummy_with_baseline = np.concatenate((np.zeros(npoints_baseline), vdummy))
+    tdummy_with_baseline = tr * np.linspace(0, npulse + npulse_offset, np.size(vdummy_with_baseline))
     
-    # solve the tof forward model
+    # define position function
+    x_func_area = partial(pfl.compute_position_numeric_spatial, tr_vect=tdummy_with_baseline, vts=vdummy_with_baseline, xarea=xarea, area=area)
+    
+    # solve the tof forward model including the extra offset pulses
+    scan_param['num_pulse'] = npulse + npulse_offset
     s_raw = run_model(x_func_area, scan_param)
     
-    # skip first 20 points
-    s_raw = s_raw[20:, :]
+    # remove inital baseline period from signal
+    s_raw = s_raw[npulse_offset:, :]
     
     # preprocess raw simulated signal 
     s = proc.scale_epi(s_raw)
     s -= np.mean(s, axis=0)
     
-    # # Add Gaussian noise
-    # mean = 0
-    # std_dev = 0.05  # Adjust the standard deviation to control the amount of noise
-    # noise = np.random.normal(mean, std_dev, (200, 3))
-    # s += noise
+    # Add zero-mean gaussian noise
+    mean = 0
+    noise = np.random.normal(mean, gauss_noise_std, (200, 3))
+    s += noise
     
+    # define ground truth velocity, same length as inflow signal
+    tdummy = tr * np.arange(0, s.shape[0])
+    velocity = np.zeros(np.size(tdummy))
+    velocity += v_offset
+    for amp, phase, w in zip(velocity_input, rand_phase, frequencies):
+        vsine = amp*np.cos(2*np.pi*w*(tdummy - phase))
+        velocity += vsine
+
+    # fill matricies
     X[idx, 0, :] = s[:, 0].squeeze()
     X[idx, 1, :] = s[:, 1].squeeze()
     X[idx, 2, :] = s[:, 2].squeeze()
     X[idx, 3, :] = xarea
     X[idx, 4, :] = area
     
-    tr = scan_param['repetition_time']
-    tdummy = tr * np.arange(0, s.shape[0]+20)
-    velocity = np.zeros(np.size(tdummy))
-    velocity += v_offset
-
-    for amp, phase, w in zip(velocity_input, rand_phase, frequencies):
-        vsine = amp*np.cos(2*np.pi*w*(tdummy - phase))
-        velocity += vsine
-
-    Y[idx, 0, :] = velocity[20:]
+    Y[idx, 0, :] = velocity
     
 
     
