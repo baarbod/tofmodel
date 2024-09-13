b2103daab1f4edcac589176405a681bb05990d0e
Baarbod Ashenagar <baarbod.ashen@gmail.com>
2024-09-13 11:52:11 -0400

    modified area input to be scaled versions of real area data

--- tofinv/simulations.py
+++ tofinv/simulations.py
@@ -2,10 +2,8 @@
 
 import multiprocessing.shared_memory as msm
 import numpy as np
-import itertools
-import inflowan.spectrum as spec
 import inflowan.processing as proc
-import inflowan.utils as inf_utils
+import tofinv.utils as utils
 from functools import partial
 from tof import posfunclib as pfl
 from tof import tofmodel as tm
@@ -25,7 +23,6 @@ def run_model(x_func, scan_param, progress=False, showplot=False):
     # run model 
     signal = tm.run_tof_model(scan_param, x_func,
                               showplot=showplot,
-                              uselookup=False,
                               progress=progress)
     s_three_slices = signal[:, 0:3]
     
@@ -37,7 +34,7 @@ def simulate_parameter_set(idx, input_data):
     # unpack tuple of inputs
     frequencies, v_offset, rand_phase, velocity_input, \
     scan_param, Xshape, Xtype, Yshape, Ytype, task_id, \
-    slc_offset, area_gauss_width, area_curve_fact, gauss_noise_std = input_data
+    xarea_sample, area_sample, gauss_noise_std = input_data
     
     tr = scan_param['repetition_time']
     npulse = scan_param["num_pulse"]
@@ -49,29 +46,15 @@ def simulate_parameter_set(idx, input_data):
     X = np.ndarray(Xshape, dtype=Xtype, buffer=Xshm.buf)
     Y = np.ndarray(Yshape, dtype=Ytype, buffer=Yshm.buf)
     
-    xoffset = slc_offset # cm
-    width = area_gauss_width # cm
-    xarea = np.linspace(-3, 3, Xshape[2]) # cm
-    N = lambda x, u, s: np.exp((-0.5) * ((x-u)/s)**2)
-    area = area_curve_fact*N(xarea, xoffset, width)
-    
-    # define velocity based fourier series parameters
-    tdummy = tr * np.linspace(0, npulse, 2000)
-    vdummy = np.zeros(np.size(tdummy))
-    vdummy += v_offset
-    for amp, phase, w in zip(velocity_input, rand_phase, frequencies):
-        vsine = amp*np.cos(2*np.pi*w*(tdummy - phase))
-        vdummy += vsine
-    
-    # add initial zero-flow period to establish baseline signal before starting flow
-    baseline_dt = tr*npulse_offset
-    dt = tdummy[1] - tdummy[0]
-    npoints_baseline = int(np.ceil(baseline_dt/dt))
-    vdummy_with_baseline = np.concatenate((np.zeros(npoints_baseline), vdummy))
-    tdummy_with_baseline = tr * np.linspace(0, npulse + npulse_offset, np.size(vdummy_with_baseline))
+    # define velocity and add initial zero-flow baseline period 
+    tdummy = tr * np.linspace(0, npulse, Yshape[2])
+    vdummy = utils.define_velocity_fourier(tdummy, velocity_input, frequencies, rand_phase, v_offset)
+    baseline_duration = tr*npulse_offset
+    tdummy_with_baseline, vdummy_with_baseline = utils.add_baseline_period(tdummy, vdummy, baseline_duration)
     
     # define position function
-    x_func_area = partial(pfl.compute_position_numeric_spatial, tr_vect=tdummy_with_baseline, vts=vdummy_with_baseline, xarea=xarea, area=area)
+    x_func_area = partial(pfl.compute_position_numeric_spatial, tr_vect=tdummy_with_baseline, 
+                          vts=vdummy_with_baseline, xarea=xarea_sample, area=area_sample)
     
     # solve the tof forward model including the extra offset pulses
     scan_param['num_pulse'] = npulse + npulse_offset
@@ -86,7 +69,7 @@ def simulate_parameter_set(idx, input_data):
     
     # Add zero-mean gaussian noise
     mean = 0
-    noise = np.random.normal(mean, gauss_noise_std, (200, 3))
+    noise = np.random.normal(mean, gauss_noise_std, (Xshape[2], 3))
     s += noise
     
     # define ground truth velocity, same length as inflow signal
@@ -101,8 +84,8 @@ def simulate_parameter_set(idx, input_data):
     X[idx, 0, :] = s[:, 0].squeeze()
     X[idx, 1, :] = s[:, 1].squeeze()
     X[idx, 2, :] = s[:, 2].squeeze()
-    X[idx, 3, :] = xarea
-    X[idx, 4, :] = area
+    X[idx, 3, :] = xarea_sample
+    X[idx, 4, :] = area_sample
     
     Y[idx, 0, :] = velocity
     
