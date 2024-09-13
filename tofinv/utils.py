72c107143ea08edc8a293dc0663e2d447f509477
Baarbod Ashenagar <baarbod.ashen@gmail.com>
2024-09-13 11:52:44 -0400

    added some functions from refactoring

--- tofinv/utils.py
+++ tofinv/utils.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import multiprocessing.shared_memory as msm
+import os
 import numpy as np
+import json
 import datetime
 import torch
 import inflowan.utils as inf_utils
+import tofinv.sampling as vd
+
 
 def create_shared_memory(original_array, name=None):
     # close if exists already
@@ -85,3 +89,66 @@ def input_batched_signal_into_NN_area(s_data_for_nn, NN_model, xarea, area, feat
 
         velocity_NN[ind1:ind2] = y_predicted
     return velocity_NN
+
+
+# load area and corresponding slice positions for a subject
+def load_area(subject, slc1, root_dir='/om/user/bashen/repositories/inflow-analysis'):
+    filepath = os.path.join(root_dir, 'data', subject, 'area.txt')
+    area = np.loadtxt(filepath) # mm^2
+    xarea = 1 * np.arange(np.size(area)) # mm
+    xarea_new = 0.1 * np.arange(10 * np.size(area)) # mm
+    area = np.interp(xarea_new, xarea, area) # mm
+    xarea = xarea_new - slc1 # mm
+    xarea *= 0.1 # convert the depth to cm
+    return xarea, area
+
+
+def get_slice_from_subject(inputs, subject, slcpos):
+    for sub in inputs:
+        if sub['name'] == subject:
+            if sub['postype'] == slcpos:
+                slc = sub['slc']
+    return slc
+
+
+def define_velocity_fourier(t, vcoeff, vfrequencies, vphase, voffset):
+    velocity = np.zeros(np.size(t))
+    velocity += voffset
+    for amp, w, phase in zip(vcoeff, vfrequencies, vphase):
+        vsine = amp*np.cos(2*np.pi*w*(t - phase))
+        velocity += vsine
+    return velocity
+
+
+def add_baseline_period(t, x, baseline_duration, baseline_value=0):
+    dt = t[1] - t[0]
+    npoints_baseline = int(np.ceil(baseline_duration/dt))
+    x_with_baseline = np.concatenate((baseline_value*np.ones(npoints_baseline), x))
+    t_with_baseline = np.linspace(0, t.max() + baseline_duration, np.size(x_with_baseline))
+    return t_with_baseline, x_with_baseline
+
+
+def load_subject_area_matrix(config_data_path, area_size):
+    with open(config_data_path, "r") as jsonfile:
+            param_data = json.load(jsonfile)
+    inputs = param_data['human_data']
+    subjects = []
+    slice_mode = []
+    for subject_info in inputs:
+        subjects.append(subject_info['name'])
+        slice_mode.append(subject_info['postype'])
+    Ay = np.zeros((area_size, len(subjects) + 1)) 
+    Ax = np.zeros((area_size, len(subjects) + 1)) 
+    for idx, (subject, slc_mode)  in enumerate(zip(subjects, slice_mode)):
+        slc = get_slice_from_subject(inputs, subject, slc_mode)
+        xarea, area = load_area(subject, slc)
+        xarea = vd.downsample(xarea, area_size)
+        area = vd.downsample(area, area_size)
+        Ax[:, idx] = xarea.flatten()
+        Ay[:, idx] = area.flatten()
+    # add straight tube case
+    subjects.append('straight_tube')
+    Ax[:, -1] = Ax[:, -2]
+    Ay[:, -1] = np.ones(Ay.shape[0])
+    
+    return Ax, Ay, subjects
