60d2cba0f68616e52bb8b750c52a604d46b46d10
Baarbod Ashenagar <baarbod.ashen@gmail.com>
2024-09-16 12:32:43 -0400

    added docstring and rearanged function order in file

--- tofmodel/forward/tofmodel.py
+++ tofmodel/forward/tofmodel.py
@@ -1,21 +1,131 @@
 # -*- coding: utf-8 -*-
 
-import math
 import time
 import numpy as np
 import matplotlib.pyplot as plt
-# from tof.fresignal import fre_signal
-from tof.fresignal import fre_signal_array as fre_signal
-
-import os
-import pickle
+from forward.fresignal import fre_signal_array as fre_signal
 
 
-def get_pulse_targets(scan_param):
+def run_tof_model(scan_param, x_func, showplot=False, progress=False):
+    """Routine for simulating inflow signals
+
+    Parameters
+    ----------
+    scan_param : dict
+        Scanning parameters
+        
+    x_func : func
+        Function of x and x0 used to define position dependance of spins
+        
+    showplot : bool, optional
+        show plots related to spin position initialization, by default False
+        
+    progress : bool, optional
+        print information about simulation progress, by default False
+
+    Returns
+    -------
+    signal : numpy.ndarray
+        matrix of signal timeseries for each slice
+    """
+    
+    
+    # define scan parameters
+    w = scan_param['slice_width']
     tr = scan_param['repetition_time']
+    fa = scan_param['flip_angle'] * np.pi / 180
+    t1 = scan_param['t1_time']
     nslice = scan_param['num_slice']
     npulse = scan_param['num_pulse']
+    multi_factor = scan_param['MBF']
     alpha = np.array(scan_param['alpha_list'], ndmin=2).T
+
+    assert np.size(alpha) == nslice, 'Warning: size of alpha should be nslice'
+
+    # set the proton initial positions
+    dx = 0.01
+    x0_array = set_init_positions(x_func, tr, w, npulse, nslice, dx, showplot=showplot, progress=progress)
+    nproton = np.size(x0_array)
+    nproton_per_slice = int(w / dx)
+
+    print('running simulation with ' + str(nproton) + ' protons...')
+
+    # find the time and target slice of each RF pulse
+    timings, pulse_slice = get_pulse_targets(tr, nslice, npulse, alpha)
+
+    # associate each pulse to its RF cycle
+    pulse_tr_actual = match_pulse_to_tr(npulse, nslice)
+
+    # get the current time before starting the simulation
+    t = time.time()
+
+    # initialize
+    signal = np.zeros([npulse, nslice])
+    s_counter = np.zeros([npulse, nslice])
+
+    # controls how often to display progress message
+    fraction = nproton / 10
+    
+    # main loop which computes proton signal contributions
+    for iproton in range(nproton):
+
+        # display progress message
+        if progress:
+            if (iproton % fraction) == 0:
+                tnow = time.time()-t
+                tstr = '{:3.2f}'.format(tnow)
+                string = str(iproton) + ' protons at ' + tstr + ' seconds'
+                print(string)
+
+        # get the initial position
+        init_pos = x0_array[iproton]
+
+        # compute position at each rf pulse
+        proton_position_no_repeats = x_func(np.unique(timings), init_pos)
+        proton_position = np.repeat(proton_position_no_repeats, multi_factor)
+
+        # convert absolute positions to slice location
+        proton_slice = np.floor(proton_position / w)
+
+        # find pulses that this proton received
+        match_cond = pulse_slice == proton_slice
+        pulse_recieve_ind = np.where(match_cond)[0]
+
+        # loop through recieved pulses and compute flow signal
+        tprev = float('nan')
+
+        # loop through each pulse and compute signal
+        dt_list = np.zeros(np.size(pulse_recieve_ind))
+        s_for_proton = []
+        for count, pulse_id in enumerate(pulse_recieve_ind):
+            tnow = timings[pulse_id]
+            dt = tnow - tprev  # correct dt behavior on 1st pulse
+            tprev = tnow
+            dt_list[count] = dt
+            npulse = count + 1  # correcting for zero-based numbering
+
+            s = fre_signal(npulse, fa, tr, t1, dt_list)
+
+            current_tr = pulse_tr_actual[pulse_id]
+            current_slice = proton_slice[pulse_id]
+            current_slice = current_slice.astype(int)
+            signal[current_tr, current_slice] += s
+            s_counter[current_tr, current_slice] += 1
+            s_for_proton.append(s)
+
+    elapsed = time.time() - t
+
+    elapsed = '{:3.2f}'.format(elapsed)
+    print('total simulation time: ' + str(elapsed) + ' seconds')
+    print('=================================================================')
+    print(' ')
+
+    # Divide after summing contributions; signal is the average of protons
+    signal = signal / nproton_per_slice
+    return signal
+
+
+def get_pulse_targets(tr, nslice, npulse, alpha):
 
     # Define list of tuples defining pulse timings and corresponding target slices
     tr_vect = np.array(range(npulse)) * tr
@@ -114,100 +224,3 @@ def set_init_positions(x_func, tr, w, npulse, nslice, dx,
         print('setting upper bound to ' + str(xupper) + ' cm')
     x0_array = np.arange(xlower, xupper, dx)
     return x0_array
-
-
-def run_tof_model(scan_param, x_func, showplot=False, progress=False):
-
-    # define scan parameters
-    w = scan_param['slice_width']
-    tr = scan_param['repetition_time']
-    fa = scan_param['flip_angle'] * math.pi / 180
-    t1 = scan_param['t1_time']
-    nslice = scan_param['num_slice']
-    npulse = scan_param['num_pulse']
-    multi_factor = scan_param['MBF']
-    alpha = np.array(scan_param['alpha_list'], ndmin=2).T
-
-    assert np.size(alpha) == nslice, 'Warning: size of alpha should be nslice'
-
-    # set the proton initial positions
-    dx = 0.01
-    x0_array = set_init_positions(x_func, tr, w, npulse, nslice, dx, showplot=showplot, progress=progress)
-    nproton = np.size(x0_array)
-    nproton_per_slice = int(w / dx)
-
-    print('running simulation with ' + str(nproton) + ' protons...')
-
-    # find the time and target slice of each RF pulse
-    timings, pulse_slice = get_pulse_targets(scan_param)
-
-    # associate each pulse to its RF cycle
-    pulse_tr_actual = match_pulse_to_tr(npulse, nslice)
-
-    # get the current time before starting the simulation
-    t = time.time()
-
-    # initialize
-    signal = np.zeros([npulse, nslice])
-    s_counter = np.zeros([npulse, nslice])
-
-    # controls how often to display progress message
-    fraction = nproton / 10
-    
-    # main loop which computes proton signal contributions
-    for iproton in range(nproton):
-
-        # display progress message
-        if progress:
-            if (iproton % fraction) == 0:
-                tnow = time.time()-t
-                tstr = '{:3.2f}'.format(tnow)
-                string = str(iproton) + ' protons at ' + tstr + ' seconds'
-                print(string)
-
-        # get the initial position
-        init_pos = x0_array[iproton]
-
-        # compute position at each rf pulse
-        proton_position_no_repeats = x_func(np.unique(timings), init_pos)
-        proton_position = np.repeat(proton_position_no_repeats, multi_factor)
-
-        # convert absolute positions to slice location
-        proton_slice = np.floor(proton_position / w)
-
-        # find pulses that this proton received
-        match_cond = pulse_slice == proton_slice
-        pulse_recieve_ind = np.where(match_cond)[0]
-
-        # loop through recieved pulses and compute flow signal
-        tprev = float('nan')
-
-        # loop through each pulse and compute signal
-        dt_list = np.zeros(np.size(pulse_recieve_ind))
-        s_for_proton = []
-        for count, pulse_id in enumerate(pulse_recieve_ind):
-            tnow = timings[pulse_id]
-            dt = tnow - tprev  # correct dt behavior on 1st pulse
-            tprev = tnow
-            dt_list[count] = dt
-            npulse = count + 1  # correcting for zero-based numbering
-
-            s = fre_signal(npulse, fa, tr, t1, dt_list)
-
-            current_tr = pulse_tr_actual[pulse_id]
-            current_slice = proton_slice[pulse_id]
-            current_slice = current_slice.astype(int)
-            signal[current_tr, current_slice] += s
-            s_counter[current_tr, current_slice] += 1
-            s_for_proton.append(s)
-
-    elapsed = time.time() - t
-
-    elapsed = '{:3.2f}'.format(elapsed)
-    print('total simulation time: ' + str(elapsed) + ' seconds')
-    print('=================================================================')
-    print(' ')
-
-    # Divide after summing contributions; signal is the average of protons
-    signal = signal / nproton_per_slice
-    return signal
