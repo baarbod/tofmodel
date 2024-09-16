11e74e012432a043781c060837e1e62b811222c3
Baarbod Ashenagar <baarbod.ashen@gmail.com>
2024-09-16 11:51:14 -0400

    updated

--- setup.py
+++ setup.py
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 
 from setuptools import find_packages, setup
 
 setup(
-    name='tof',
+    name='tofmodel',
     packages=find_packages(),
-=======
-# -*- coding: utf-8 -*-
-
-from setuptools import find_packages, setup
-
-setup(
-    name='tofinv',
-    packages=find_packages(),
->>>>>>> repo-b/master
 )
