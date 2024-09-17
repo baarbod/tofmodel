31d3906b1f57a0c97cad026064dc0e6d165adee2
Baarbod Ashenagar <baarbod.ashen@gmail.com>
2024-09-17 12:16:13 -0400

    moved in functions, and added docstrings

--- tofmodel/inverse/utils.py
+++ tofmodel/inverse/utils.py
@@ -1,16 +1,28 @@
 # -*- coding: utf-8 -*-
 
 import multiprocessing.shared_memory as msm
-import os
 import numpy as np
-import json
 import datetime
 import torch
-import inflowan.utils as inf_utils
-import tofinv.sampling as vd
 
 
 def create_shared_memory(original_array, name=None):
+    """ Create a shared memory array than can be used across multiple CPU
+
+    Parameters
+    ----------
+    original_array : numpy.ndarray
+        input array
+        
+    name : str, optional
+        string to use as the shared arrays name, by default None
+
+    Returns
+    -------
+    shm: multiprocessing.shared_memory.SharedMemory
+        shared memory array
+    """
+    
     # close if exists already
     close_shared_memory(name=name)
     
@@ -22,6 +34,14 @@ def create_shared_memory(original_array, name=None):
 
 
 def close_shared_memory(name=None):
+    """ Close a shared memory variable
+
+    Parameters
+    ----------
+    name : str, optional
+        name of shared memory to close, by default None
+    """
+    
     try:
         shm = msm.SharedMemory(name=name)
         shm.close()
@@ -31,6 +51,21 @@ def close_shared_memory(name=None):
 
 
 def get_shared_array(name: str, shape=None):
+    """ Get the numpy array from a shared memory array
+
+    Parameters
+    ----------
+    name : str
+        name of shared memory variable
+    shape : tuple, optional
+        shape of numpy array, by default None
+
+    Returns
+    -------
+    np_array: numpy.ndarray
+        numpy array taken from shared memory array
+    """
+    
     mp_array = globals()[name]
     np_array = np.frombuffer(mp_array.get_obj(), dtype=np.dtype(mp_array.get_obj()._type_))
     if (shape is None) and (name + '_shape' in globals().keys()):
@@ -39,32 +74,155 @@ def get_shared_array(name: str, shape=None):
     if shape is not None:
         np_array = np_array.reshape(shape)
     return np_array
+
+
+def define_velocity_fourier(t, vcoeff, vfrequencies, vphase, voffset):
+    """ Define a velocity vector based on fourier series parameters
+
+    Parameters
+    ----------
+    t : numpy.ndarray
+        time points to evaluate fourier series
+        
+    vcoeff : list
+        fourier frequency coeffiecients (cm/s)
+        
+    vfrequencies : list
+        frequencies (Hz)
+        
+    vphase : list
+        time-shifts (s) 
+        
+    voffset : float
+        offset velocity (cm/s)
+
+    Returns
+    -------
+    velocity: numpy.ndarray
+        evaluated velocity vector (cm/s)
+    """
     
+    velocity = np.zeros(np.size(t))
+    velocity += voffset
+    for amp, w, phase in zip(vcoeff, vfrequencies, vphase):
+        vsine = amp*np.cos(2*np.pi*w*(t - phase))
+        velocity += vsine
+    return velocity
+
+
+def add_baseline_period(t, x, baseline_duration, baseline_value=0.0):
+    """ Add a period of baseline to the beginning of a timeseries
+
+    Parameters
+    ----------
+    t : numpy.ndarray
+        input time vector
+        
+    x : numpy.ndarray
+        input signal vector
+        
+    baseline_duration : float
+        number of seconds of the baseline period
+        
+    baseline_value : float, optional
+        baseline value, by default 0
+
+    Returns
+    -------
+    t_with_baseline: 
+        time vector with added baseline
+    
+    x_with_baseline: 
+        signal vector with added baseline
+        
+    """
+    
+    dt = t[1] - t[0]
+    npoints_baseline = int(np.ceil(baseline_duration/dt))
+    x_with_baseline = np.concatenate((baseline_value*np.ones(npoints_baseline), x))
+    t_with_baseline = np.linspace(0, t.max() + baseline_duration, np.size(x_with_baseline))
+    return t_with_baseline, x_with_baseline
+
+
+def smooth(y, box_pts):
+    """ Smooth signal
+
+    Parameters
+    ----------
+    y : numpy.ndarry
+        input signal 
+        
+    box_pts : int
+        size of smoothing box kernal
+
+    Returns
+    -------
+    y_smooth: numpy.ndarry
+        smoothing version input signal with the same dimensions
+    """
+    
+    box = np.ones(box_pts)/box_pts
+    y_smooth = np.convolve(y, box, mode='same')
+    return y_smooth
+
+
+def upsample(y_input, n, tr):
+    """ Upsample a signal using interpolation
+
+    Parameters
+    ----------
+    y_input : numpy.ndarray
+        input signal
+        
+    n : int
+        number of timepoints in upsampled signal
+
+    Returns
+    -------
+    y_interp: numpy.ndarray
+        signal upsampled to the new size
+    """
+    
+    # increase sampling rate using linear interpolation
+    if y_input.ndim == 1:
+        y_input = np.expand_dims(y_input, 0).T
+    npoints, ncols = np.shape(y_input)
+    y_interp = np.zeros((n, ncols))
+    x = tr * np.arange(npoints)
+    for icol in range(ncols):
+        y = y_input[:, icol]
+        xvals = np.linspace(np.min(x), np.max(x), n)
+        y_interp[:, icol] = np.interp(xvals, x, y)
+    return y_interp
+
+
+def downsample(data, new_size):
+    """ Downsample a 1d signal using interpolation
+
+    Parameters
+    ----------
+    data : numpy.ndarray
+        input signal
+        
+    new_size : int
+        number of elements in downsampled signal
+
+    Returns
+    -------
+    downsampled_vector: numpy.ndarray
+        signal downsampled to the new size   
+    """
+    
+    old_size = np.size(data)
+    new_indices = np.linspace(0, old_size - 1, new_size)
+    downsampled_vector = np.interp(new_indices, np.arange(old_size), data)
+    return downsampled_vector
+
 
 def get_formatted_day_time():
     now = datetime.datetime.now()
     formatted_datetime = now.strftime("%Y-%m-%d_%H:%M:%S")
     return formatted_datetime
-
-
-def load_data(subject, fmriruns, pcruns, mctimes=None, subdir='data/measured'):
-    # combine pc runs
-    if subject == 'ff07':
-        v = inf_utils.combine_pc_runs(subject, pcruns, dofilter=False, delimiter=None, subdir=subdir)
-    else:
-        v = inf_utils.combine_pc_runs(subject, pcruns, dofilter=False, delimiter=',', subdir=subdir)
-
-    # combine epi runs
-    if subject == 'fmv04' or subject == 'ff07':
-        s_data = inf_utils.combine_epi_runs(subject, fmriruns, startind=40, delimiter=None, remove_offset=True, subdir=subdir)
-    else:
-        s_data = inf_utils.combine_epi_runs(subject, fmriruns, delimiter=',', startind=40, remove_offset=True, subdir=subdir)
-
-    if mctimes:
-        s_raw = inf_utils.remove_motion_events(s_data, mctimes[0], mctimes[1])
-    else:
-        s_raw = s_data
-    return s_raw, v
 
 
 def input_batched_signal_into_NN_area(s_data_for_nn, NN_model, xarea, area, feature_length=200):
@@ -89,66 +247,3 @@ def input_batched_signal_into_NN_area(s_data_for_nn, NN_model, xarea, area, feat
 
         velocity_NN[ind1:ind2] = y_predicted
     return velocity_NN
-
-
-# load area and corresponding slice positions for a subject
-def load_area(subject, slc1, root_dir='/om/user/bashen/repositories/inflow-analysis'):
-    filepath = os.path.join(root_dir, 'data', subject, 'area.txt')
-    area = np.loadtxt(filepath) # mm^2
-    xarea = 1 * np.arange(np.size(area)) # mm
-    xarea_new = 0.1 * np.arange(10 * np.size(area)) # mm
-    area = np.interp(xarea_new, xarea, area) # mm
-    xarea = xarea_new - slc1 # mm
-    xarea *= 0.1 # convert the depth to cm
-    return xarea, area
-
-
-def get_slice_from_subject(inputs, subject, slcpos):
-    for sub in inputs:
-        if sub['name'] == subject:
-            if sub['postype'] == slcpos:
-                slc = sub['slc']
-    return slc
-
-
-def define_velocity_fourier(t, vcoeff, vfrequencies, vphase, voffset):
-    velocity = np.zeros(np.size(t))
-    velocity += voffset
-    for amp, w, phase in zip(vcoeff, vfrequencies, vphase):
-        vsine = amp*np.cos(2*np.pi*w*(t - phase))
-        velocity += vsine
-    return velocity
-
-
-def add_baseline_period(t, x, baseline_duration, baseline_value=0):
-    dt = t[1] - t[0]
-    npoints_baseline = int(np.ceil(baseline_duration/dt))
-    x_with_baseline = np.concatenate((baseline_value*np.ones(npoints_baseline), x))
-    t_with_baseline = np.linspace(0, t.max() + baseline_duration, np.size(x_with_baseline))
-    return t_with_baseline, x_with_baseline
-
-
-def load_subject_area_matrix(config_data_path, area_size):
-    with open(config_data_path, "r") as jsonfile:
-            param_data = json.load(jsonfile)
-    inputs = param_data['human_data']
-    subjects = []
-    slice_mode = []
-    for subject_info in inputs:
-        subjects.append(subject_info['name'])
-        slice_mode.append(subject_info['postype'])
-    Ay = np.zeros((area_size, len(subjects) + 1)) 
-    Ax = np.zeros((area_size, len(subjects) + 1)) 
-    for idx, (subject, slc_mode)  in enumerate(zip(subjects, slice_mode)):
-        slc = get_slice_from_subject(inputs, subject, slc_mode)
-        xarea, area = load_area(subject, slc)
-        xarea = vd.downsample(xarea, area_size)
-        area = vd.downsample(area, area_size)
-        Ax[:, idx] = xarea.flatten()
-        Ay[:, idx] = area.flatten()
-    # add straight tube case
-    subjects.append('straight_tube')
-    Ax[:, -1] = Ax[:, -2]
-    Ay[:, -1] = np.ones(Ay.shape[0])
-    
-    return Ax, Ay, subjects
