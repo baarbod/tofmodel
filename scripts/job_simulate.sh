2c35ed994d1af0f3859f254d983eb45f8e03d3d3
Baarbod Ashenagar <baarbod.ashen@gmail.com>
2024-09-18 10:50:05 -0400

    changed paths

--- scripts/job_simulate.sh
+++ scripts/job_simulate.sh
@@ -8,8 +8,8 @@
 #SBATCH --mail-user=bashen@bu.edu
 #SBATCH --mail-type=BEGIN,END
 
-#SBATCH --output=/om/user/bashen/repositories/tof-inverse/data/simulated/ongoing/output_%A_%a.txt
-#SBATCH --error=/om/user/bashen/repositories/tof-inverse/data/simulated/ongoing/error_%A_%a.txt  
+#SBATCH --output=/om/user/bashen/repositories/tofmodel/data/simulated/ongoing/output_%A_%a.txt
+#SBATCH --error=/om/user/bashen/repositories/tofmodel/data/simulated/ongoing/error_%A_%a.txt  
 
-config_path=/om/user/bashen/repositories/tof-inverse/config/config.json
-python simulate.py $SLURM_ARRAY_TASK_ID $config_path
+config_path=/om/user/bashen/repositories/tofmodel/config/config.json
+python generate_dataset.py $SLURM_ARRAY_TASK_ID $config_path
