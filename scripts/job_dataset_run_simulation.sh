a1ebb00a603f1e3ef5d6436d15d12aeb666ae0bc
Baarbod Ashenagar <baarbod.ashen@gmail.com>
2024-09-20 15:37:29 -0400

    initial commit

---
+++ scripts/job_dataset_run_simulation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash                      
+#SBATCH -t 02:00:00         
+#SBATCH -N 1 
+#SBATCH -n 20               
+#SBATCH --mem=32G                
+#SBATCH --job-name=runsim
+#SBATCH --array=1-100%15  
+#SBATCH --mail-user=bashen@bu.edu
+#SBATCH --mail-type=BEGIN,END
+
+#SBATCH --output=/om/user/bashen/repositories/tofmodel/scripts/logs/stdout_%x_%A.txt
+#SBATCH --error=/om/user/bashen/repositories/tofmodel/scripts/logs/stderr_%x_%A.txt
+#SBATCH --open-mode=append
+
+config_path=/om/user/bashen/repositories/tofmodel/config/config.json
+python /om/user/bashen/repositories/tofmodel/tofmodel/inverse/dataset.py $SLURM_ARRAY_TASK_ID $config_path run_simulation
