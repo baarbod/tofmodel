014ca05983b91658b34f86ca31456806062f6a18
Baarbod Ashenagar <baarbod.ashen@gmail.com>
2024-09-20 15:43:10 -0400

    deleted

--- scripts/job_combine_batches.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash                      
-#SBATCH -t 1:00:00         
-#SBATCH -n 1                
-#SBATCH --mem=64G                      
-#SBATCH --job-name=combine
-#SBATCH --output=output_%A_%a.txt  
-#SBATCH --error=error_%A_%a.txt  
-
-python combine_batches.py
--- scripts/job_simulation_pipeline.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash     
-
-
-jid1=`sbatch job_simulate.sh | awk '{ print $4 }'`    # Submit job array 1 and get the job ID.
-
-sbatch --dependency=afterok:$jid1 job_combine_batches.sh     # Job 2 will start after job array 1 succeeds.
