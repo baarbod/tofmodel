6c71647719154a247693c58c7d17ffa6da3535fa
Baarbod Ashenagar <baarbod.ashen@gmail.com>
2025-05-07 16:41:11 -0400

    added entry for cli tool

--- setup.py
+++ setup.py
@@ -5,4 +5,9 @@ from setuptools import find_packages, setup
 setup(
     name='tofmodel',
     packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'tof = tofmodel.cli:main',
+        ],
+    }
 )
