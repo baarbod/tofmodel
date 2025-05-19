9a1dc8c3b7361ea75f0d4dce5b8ecd3a21249878
Baarbod Ashenagar <baarbod.ashen@gmail.com>
2025-05-19 15:28:11 -0400

    moved data scaling here, modularized, removed argparse

--- tofmodel/inverse/train.py
+++ tofmodel/inverse/train.py
@@ -8,66 +8,104 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import os
 import pickle
-import json
-import argparse
 import time 
 from tofmodel.inverse.models import TOFinverse
 import tofmodel.inverse.utils as utils
-from tofmodel.path import ROOT_DIR
 
 
-parser = argparse.ArgumentParser(description='Script for training neural network on simulated dataset')
-parser.add_argument('--datafolder', help='path to folder containing simulated dataset and config file')
-parser.add_argument('--epochs', default=30, type=int, help='number of total epochs to run (default: 10)')
-parser.add_argument('--batch', default=16, type=int, help='batch size (default: 16)')
-parser.add_argument('--lr', default=0.001, type=float, help='initial learning rate (default: 0.1)')
-args = parser.parse_args()
+def train_net(param):
 
-sim_dataset_path = os.path.join(ROOT_DIR, "data", "simulated", args.datafolder)
-config_path = os.path.join(sim_dataset_path, "config_used.json")    
-with open(config_path, "r") as jsonfile:
-    param = json.load(jsonfile)
+    # param['training_data'] = training_data
+    X_train, X_test, y_train, y_test = load_dataset(param.paths.datasetdir)
+    train_dataset = torch.utils.data.TensorDataset(X_train, y_train)
+    test_dataset = torch.utils.data.TensorDataset(X_test, y_test)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=param.training.batch, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=param.training.batch, shuffle=False)
 
-# find simulated dataset pickle file
-for file in os.listdir(sim_dataset_path):
-    if file.endswith(".pkl"):
-            training_data_filename = file
-            
-# load training data set 
-training_data = os.path.join(sim_dataset_path, training_data_filename)    
-with open(training_data, "rb") as f:
-    X, y, _ = pickle.load(f)
+    # initialize the model and optimizer
+    feature_size = X_train.shape[2]
+    output_size = y_train.shape[2]
+    model = TOFinverse(param.data_simulation.num_input_features, feature_size, output_size)
+    optimizer = optim.Adam(model.parameters(), lr=param.training.lr)
+    criterion = nn.MSELoss()
+    print(model)
+
+    # training loop
+    train_losses = []
+    test_losses = []
+    time_start = time.time()
     
-X = X.astype(float)
-y = y.astype(float)
+    for epoch in range(param.training.epochs):
+        train_loss = train(train_loader, model, criterion, optimizer)
+        test_loss = test(test_loader, model, criterion)
+        
+        train_losses.append(train_loss)
+        test_losses.append(test_loss)
+        print(f'Epoch [{epoch+1}/{param.training.epochs}], Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}')
+
+    time_end = time.time()
+    training_time = time_end - time_start
+    print(f"Total time for training loop: {training_time:.4f} seconds")
+
+    # plot loss vs. epoch for training and evaluation data
+    fig_loss, ax= plt.subplots()
+    ax.plot(train_losses, label='train data')
+    with torch.no_grad():
+        ax.plot(np.array(test_losses), label='test data')
+    ax.set_xlabel('Epoch')
+    ax.set_ylabel('Loss')
+    ax.legend()
+    ax.grid()
+    ax.xaxis.set_ticks(np.arange(0, param.training.epochs, 4))
+
+    # evaluate model accuracy using test data
+    model.eval()
+    with torch.no_grad():
+        outputs = model(X_test)
+        MSE = ((torch.pow((outputs - y_test[:, 0, :]), 2)).sum()) / outputs.numel()
+        print(f'Mean Squared Error: {MSE:.4f}')
+
+    # create folder associated with this simulation
+    folder_root = os.path.join(param.paths.datasetdir, "experiments")
+    formatted_datetime =  utils.get_formatted_day_time()
+    folder_name = f"{formatted_datetime}_training_run"
+    folder = os.path.join(folder_root, folder_name)
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    # save plot of MSE loss vs. epochs
+    figpath = os.path.join(folder, 'MSE_loss_vs_epochs.png')
+    fig_loss.savefig(figpath, format='png')
+
+    # save training information
+    num_samples = X_train.shape[0]
+    output_path = os.path.join(folder, f"model_state_{num_samples}_samples.pth")
+    print('Saving  training experiment...')   
+    torch.save(model.state_dict(), output_path)
+    print('Finished saving.')   
+
+
+def load_dataset(path):
+    pkl_file = next(f for f in os.listdir(path) if f.endswith('.pkl'))
+    with open(os.path.join(path, pkl_file), "rb") as f:
+        X, y, _ = pickle.load(f)
+    X, y = X.astype(float), y.astype(float)
+        
+    # perform data augmentations
+    for i in range(X.shape[0]):
+        s_raw = X[i, :3, :].T           
+        s = utils.scale_epi(s_raw)         
+        s -= np.mean(s, axis=0)       
+        X[i, :3, :] = s.T
     
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
-param['training_data'] = training_data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
+    X_train = torch.tensor(X_train, dtype=torch.float32)
+    y_train = torch.tensor(y_train, dtype=torch.float32)
+    X_test = torch.tensor(X_test, dtype=torch.float32)
+    y_test = torch.tensor(y_test, dtype=torch.float32)
 
-# create an instance of the TOFinverse model
-nfeatures = param['data_simulation']['num_input_features']
-feature_size = X.shape[2]
-output_size = y.shape[2]  # length of output velocity spectrum
-model = TOFinverse(nfeatures, feature_size, output_size)
-print(model)
+    return X_train, X_test, y_train, y_test
 
-# convert data to PyTorch tensors
-X_train = torch.tensor(X_train, dtype=torch.float32)
-y_train = torch.tensor(y_train, dtype=torch.float32)
-X_test = torch.tensor(X_test, dtype=torch.float32)
-y_test = torch.tensor(y_test, dtype=torch.float32)
-
-# create data loader objects
-train_dataset = torch.utils.data.TensorDataset(X_train, y_train)
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch, shuffle=True)
-test_dataset = torch.utils.data.TensorDataset(X_test, y_test)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=args.batch, shuffle=False)
-
-# initialize the model and optimizer
-model = TOFinverse(nfeatures=nfeatures, feature_size=X_train.shape[2],
-                   output_size=output_size)
-optimizer = optim.Adam(model.parameters(), lr=args.lr)
-criterion = nn.MSELoss()
 
 def train(loader, model, criterion, optimizer):
     model.train()
@@ -94,64 +132,3 @@ def test(loader, model, criterion):
     avg_loss = epoch_loss / len(loader.dataset)
     return avg_loss
 
-
-# training loop
-train_losses = []
-test_losses = []
-time_start = time.time()
-for epoch in range(args.epochs):
-
-    train_loss = train(train_loader, model, criterion, optimizer)
-    test_loss = test(test_loader, model, criterion)
-    
-    train_losses.append(train_loss)
-    test_losses.append(test_loss)
-    print(f'Epoch [{epoch+1}/{args.epochs}], Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}')
-
-time_end = time.time()
-training_time = time_end - time_start
-print(f"Total time for training loop: {training_time:.4f} seconds")
-
-# plot loss vs. epoch for training and evaluation data
-fig_loss, ax= plt.subplots()
-ax.plot(train_losses, label='train data')
-with torch.no_grad():
-    ax.plot(np.array(test_losses), label='test data')
-ax.set_xlabel('Epoch')
-ax.set_ylabel('Loss')
-ax.legend()
-ax.grid()
-ax.xaxis.set_ticks(np.arange(0, args.epochs, 4))
-
-# evaluate model accuracy using test data
-model.eval()
-with torch.no_grad():
-    outputs = model(X_test)
-    MSE = ((torch.pow((outputs - y_test[:, 0, :]), 2)).sum()) / outputs.numel()
-    print(f'Mean Squared Error: {MSE:.4f}')
-
-param['MSE'] = float(MSE)
-# create folder associated with this simulation
-folder_root = os.path.join(sim_dataset_path, "experiments")
-formatted_datetime =  utils.get_formatted_day_time()
-project_name = param['info']['name']
-folder_name = f"{formatted_datetime}_training_run_{project_name}"
-folder = os.path.join(folder_root, folder_name)
-if not os.path.exists(folder):
-   os.makedirs(folder)
-
-# save plot of MSE loss vs. epochs
-figpath = os.path.join(folder, 'MSE_loss_vs_epochs.png')
-fig_loss.savefig(figpath, format='png')
-
-# log config file used for training
-config_path = os.path.join(folder, 'config_used.json')
-with open(config_path, 'w') as fp:
-    json.dump(param, fp, indent=4)
-
-# save training information
-num_samples = X.shape[0]
-output_path = os.path.join(folder, f"model_state_{num_samples}_samples.pth")
-print('Saving  training experiment...')   
-torch.save(model.state_dict(), output_path)
-print('Finished saving.')   
