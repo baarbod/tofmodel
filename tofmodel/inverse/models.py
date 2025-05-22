a7775379522660bdaff0f49de9ae4ac32fa2bbc8
Baarbod Ashenagar <baarbod.ashen@gmail.com>
2025-05-22 15:05:23 -0400

    allow for more than 1 output feature

--- tofmodel/inverse/models.py
+++ tofmodel/inverse/models.py
@@ -34,18 +34,21 @@ class Up(nn.Module):
     
 class TOFinverse(nn.Module):
     
-    def __init__(self, nfeatures, feature_size, output_size):
+    def __init__(self, nfeature_in, nfeature_out, input_size, output_size):
         super(TOFinverse, self).__init__()
         
-        self.down1 = (Down(nfeatures, 16))
+        self.down1 = (Down(nfeature_in, 16))
         self.down2 = (Down(16, 32))
         self.down3 = (Down(32, 64))
         self.up1 = (Up(64, 64))
         self.up2 = (Up(64, 32))
         self.up3 = (Up(32, 16))
+
+        self.output_size = output_size
+        self.nfeature_out = nfeature_out
         
-        self.fc1 = nn.Linear(16 * feature_size, 800)
-        self.fc2 = nn.Linear(800, output_size)
+        self.fc1 = nn.Linear(16 * input_size, 800)
+        self.fc2 = nn.Linear(800, nfeature_out * output_size)
 
     def forward(self, x):
         x = self.down1(x)
@@ -57,24 +60,7 @@ class TOFinverse(nn.Module):
         x = x.view(x.size(0), -1)  # Flatten the tensor
         x = self.fc1(x)
         x = self.fc2(x)
+        x = x.view(x.size(0), self.nfeature_out, self.output_size) 
         return x
 
-
-
-class TOFinverse0(nn.Module):
-
-    def __init__(self, nfeatures, feature_size, output_size):
-        super(TOFinverse0, self).__init__()
-        self.conv1 = nn.Conv1d(in_channels=nfeatures, out_channels=16, kernel_size=3, padding=1)
-        self.relu = nn.ReLU()
-        self.fc1 = nn.Linear(16 * feature_size, 128)
-        self.fc2 = nn.Linear(128, output_size)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.relu(x)
-        x = x.view(x.size(0), -1)  # Flatten the tensor
-        x = self.fc1(x)
-        x = self.fc2(x)
-        return x
     
