bb91fba0a31d1cedccaa2b9b85b81d427bb59531
Baarbod Ashenagar <baarbod.ashen@gmail.com>
2025-05-24 13:33:58 -0400

    optimized for speed

--- tofmodel/forward/fresignal.py
+++ tofmodel/forward/fresignal.py
@@ -3,6 +3,7 @@
 import numpy as np
 import operator
 import functools
+from numba import njit
 
 
 # array version can run faster for slower flows, but perform same or worse for
@@ -31,37 +32,41 @@ def fre_signal(n, fa, tr, t1, dt_list):
     return s
 
 
-# Define equation for flow-enhanced fMRI signal using array method
+# Define equation for flow-enhanced fMRI signal using array method (optimized)
+@njit
 def fre_signal_array(n, fa, tr, t1, dt_list, offset_fact=1):
     
     # need to implement this properly at some point
     TE = 30/1000
     T2 = 1.5
-    
     m0 = 1
-    c = np.cos(fa)
-    mz_ss = offset_fact * m0 * (1 - np.exp(-tr / t1)) / (1 - np.exp(-tr / t1) * c)
-    exponentials = np.exp(-dt_list / t1)
-
-    exponentials_full = np.prod(exponentials[1:n])
+    sin_fa = np.sin(fa)
+    cos_fa = np.cos(fa)
+    exp_tr_t1 = np.exp(-tr / t1)
+    exp_te_t2 = np.exp(-TE / T2)
+    mz_ss = offset_fact * m0 * (1 - exp_tr_t1) / (1 - exp_tr_t1 * cos_fa)
     
-    # put all terms in array
-    full_array = np.ones((n - 1, n - 1))
-    for m in range(n - 1):
-        full_array[m, 0:n - m - 1] = exponentials[1:n - m]
+    exponentials = np.empty(n)
+    for i in range(n):
+        exponentials[i] = np.exp(-dt_list[i] / t1)
+
+    exponentials_full = 1.0
+    for i in range(1, n):
+        exponentials_full *= exponentials[i]
 
     if n == 1:
-        series = 0
-        mzn_pre = m0 * exponentials_full * (np.sum(series) + c ** (n - 1))
-        s = np.sin(fa) * (mzn_pre - mz_ss)
+        mzn_pre = m0 * exponentials_full
+        s = sin_fa * exp_te_t2 * (mzn_pre - mz_ss)
     else:
-        # call prod function on full array
-        full_array_prod = np.prod(full_array.T, axis=0)
-        mm = np.arange(0, n - 1)
-        nummat = 1 - exponentials[n - mm - 1]
-        final_mat = c ** mm * nummat / full_array_prod
-        series = final_mat
-        mzn_pre = m0 * exponentials_full * (np.sum(series) + c ** (n - 1))
-        s = np.sin(fa) * np.exp(-TE/T2) * (mzn_pre - mz_ss)
+        series_sum = 0.0
+        for mm in range(n - 1):
+            num = 1.0 - exponentials[n - mm - 1] 
+            prod = 1.0
+            for k in range(1, n - mm):
+                prod *= exponentials[k]
+            series_sum += (cos_fa ** mm) * num / prod
+
+        mzn_pre = m0 * exponentials_full * (series_sum + cos_fa ** (n - 1))
+        s = sin_fa * exp_te_t2 * (mzn_pre - mz_ss)
 
     return s
