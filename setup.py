e0578d8c12ca52d268167cbde912cf4bfb22c023
Baarbod Ashenagar <baarbod.ashen@gmail.com>
2025-07-03 13:41:24 -0400

    version 2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='tofmodel',
-    version='1.1',
+    version='2.0',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
