48817a345d722276a9325ee1498e1710b29e07c6
Baarbod Ashenagar <baarbod.ashen@gmail.com>
2025-09-27 17:42:51 -0400

    modified architecture to improve temporal allignment]

--- tofmodel/inverse/models.py
+++ tofmodel/inverse/models.py
@@ -1,54 +1,31 @@
-# -*- coding: utf-8 -*-
-
 import torch.nn as nn
-import torch as torch 
 
 
-class Down(nn.Module):
-    def __init__(self, in_channels, out_channels):
+class Conv(nn.Module):
+    def __init__(self, in_channels, out_channels, dilation=1):
         super().__init__()
-        self.conv = nn.Conv1d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, padding=1)
+        self.conv = nn.Conv1d(in_channels, out_channels,
+                              kernel_size=3, padding=dilation, dilation=dilation)
         self.norm = nn.BatchNorm1d(out_channels)
         self.relu = nn.ReLU()
 
     def forward(self, x):
-        x = self.conv(x)
-        x = self.norm(x)
-        x = self.relu(x)
-        return x
+        return self.relu(self.norm(self.conv(x)))
 
 
-class Up(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.conv = nn.ConvTranspose1d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, padding=1)
-        self.norm = nn.BatchNorm1d(out_channels)
-        self.relu = nn.ReLU()
-
-    def forward(self, x):
-        x = self.conv(x)
-        x = self.norm(x)
-        x = self.relu(x)
-        return x
-    
-    
 class TOFinverse(nn.Module):
-    
     def __init__(self, nfeature_in, nfeature_out, input_size, output_size):
-        super(TOFinverse, self).__init__()
-        
-        self.down1 = (Down(nfeature_in, 16))
-        self.down2 = (Down(16, 32))
-        self.down3 = (Down(32, 64))
-        self.up1 = (Up(64, 64))
-        self.up2 = (Up(64, 32))
-        self.up3 = (Up(32, 16))
+        super().__init__()
+
+        self.down1 = Conv(nfeature_in, 16, dilation=1)
+        self.down2 = Conv(16, 32, dilation=2)
+        self.down3 = Conv(32, 64, dilation=4)
+        self.up1 = Conv(64, 32, dilation=2)
+        self.up2 = Conv(32, 16, dilation=1)
+        self.out = nn.Conv1d(16, nfeature_out, kernel_size=1)
 
         self.output_size = output_size
         self.nfeature_out = nfeature_out
-        
-        self.fc1 = nn.Linear(16 * input_size, 800)
-        self.fc2 = nn.Linear(800, nfeature_out * output_size)
 
     def forward(self, x):
         x = self.down1(x)
@@ -56,11 +33,5 @@ class TOFinverse(nn.Module):
         x = self.down3(x)
         x = self.up1(x)
         x = self.up2(x)
-        x = self.up3(x)
-        x = x.view(x.size(0), -1)  # Flatten the tensor
-        x = self.fc1(x)
-        x = self.fc2(x)
-        x = x.view(x.size(0), self.nfeature_out, self.output_size) 
+        x = self.out(x)
         return x
-
-    
